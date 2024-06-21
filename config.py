53efba00aba4a2a0bff337b235e5bc78ded78307
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2024-06-21 22:29:00 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 source_urls = [
-    "http://xn--ihqu10cn4c.xn--v4q818bf34b.top/live.txt",
-    "https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1716213337323/live1.txt",
-    "https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1716213337323/live2.txt",
-    "https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1716213337323/live3.txt",
     "http://120.79.4.185/new/mdlive.txt",
     "https://tv.youdu.fan:666/live/",
     "http://175.178.251.183:6689/livv.txt",
     "https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt",
+    "https://raw.githubusercontent.com/Tzwcard/ChinaTelecom-GuangdongIPTV-RTP-List/master/GuangdongIPTV_rtp_sd.m3u",
+    "https://raw.githubusercontent.com/YueChan/Live/main/IPTV.m3u",
     "https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1715581924111/live1.txt",
     "http://rihou.cc:88/hccx/zb.txt",
     "https://live.goodiptv.club/api/getlive.php",
     "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
     "https://fanmingming.com/txt?url=https://raw.githubusercontent.com/hiyoi/IPTV/main/IPTV.m3u",
+    "https://raw.githubusercontent.com/iptv-pro/iptv-pro.github.io/main/tv/iptv.m3u",
+    "https://raw.githubusercontent.com/iptv-pro/iptv-pro.github.io/main/list.txt",
     "https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
     "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt"
 ]
